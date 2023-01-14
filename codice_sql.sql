06086cf0a661652f3528aca074a22b81bede4303
unknown <favaretto801@gmail.com>
2023-01-14 15:46:58 +0100

    codice sql

---
+++ codice_sql.sql
@@ -0,0 +1,99 @@
+--drop table in caso di tabelle già esistenti
+
+drop table if exists Galleria;
+drop table if exists Sala;
+drop table if exists Opera;
+drop table if exists Artista;
+drop table if exists NegozioSouvenir;
+drop table if exists Evento;
+drop table if exists Prodotto;
+drop table if exists Vende;
+drop table if exists Dipendente;
+
+--create ed alter table del database
+
+create table Galleria (
+    Luogo varchar(50) not null,
+    SpazioDisponibile int,
+    Nome varchar(50) not null,
+    primary key(Luogo)
+);
+
+create table Sala (
+    Nome varchar(20) not null
+    NumeroEspositori int not null,
+    Luogo varchar(50) not null,
+    foreign key (Luogo) references Galleria(Luogo) on update cascade on delete cascade
+    primary key(Nome, Luogo)
+);
+
+create table Artista (
+    NickName varchar(30) not null,
+    DataNascita DATE not null,
+    Nome varchar(30),
+    primary key(NickName)
+);
+
+create table Opera (
+    ID int not null,
+    Titolo varchar(50) not null,
+    Descrizione varchar(500),
+    Tipo varchar(50) not null,
+    Luogo varchar(50) not null,
+    Nome varchar(20) not null,
+    Autore varchar(30) not null,
+    foreign key (Luogo) references Sala(Luogo) on update cascade on delete cascade,
+    foreign key (Nome) references Sala(Nome) on update cascade on delete cascade,
+    foreign key (Autore) references Artista(NickName) on update cascade on delete cascade,
+    primary key(ID)
+);
+
+create table Evento (
+    ID int not null,
+    Nome varchar(60) not null, 
+    DataInizio DATE not null,
+    DataFine DATE not null,
+    Luogo varchar(50) not null,
+    foreign key (Luogo) references Galleria(Luogo) on update cascade on delete cascade,
+    primary key (ID)
+);
+
+create table NegozioSouvenir (
+    Orario DATE not null,
+    Luogo varchar(50) not null,
+    foreign key (Luogo) references Galleria(Luogo) on update cascade on delete cascade,
+    primary key (Luogo)
+);
+
+create table Prodotto (
+    TipoProdotto varchar(50) not null,
+    Disponibilita boolean not null default = false
+    primary key (TipoProdotto)
+);
+
+create table Vende (
+    Luogo varchar(50) not null,
+    TipoProdotto varchar(50) not null,
+    foreign key (Luogo) references NegozioSouvenir(Luogo) on update cascade on delete cascade,
+    foreign key (TipoProdotto) references Prodotto(TipoProdotto) on update cascade on delete cascade,
+    primary key (Luogo, TipoProdotto)
+);
+
+create table Dipendente (
+    CodiceFiscale varchar(60) not null,
+    Salario int not null,
+    Mansione varchar(20),
+    Galleria varchar(20) not null,
+    foreign key (Galleria) references Galleria(Luogo) on update cascade on delete cascade,
+    primary key(CodiceFiscale)
+);
+
+
+--Insert values del datbase
+
+
+--Query
+
+
+--Indici
+
