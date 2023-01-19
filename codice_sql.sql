bdd0c0b20ddade176b0444fb47ddb49a99002894
elenam001 <elena.marchioro.4@studenti.unipd.it>
2023-01-19 16:44:34 +0100

    un po' di insert

--- codice_sql.sql
+++ codice_sql.sql
@@ -89,38 +89,45 @@ create table Dipendente (
 
 
 --Insert values del datbase
-insert into Galleria(Luogo, Nome)
-values (Manhattan, NYGallery);
-values (Bronx, BGallery)
-values (Brooklin, KGallery);
-values (Queens, QGallery);
-values (Staten Island, RGallery)
+insert into Galleria(Luogo, Nome) values
+ (Manhattan, NYGallery);
+ (Bronx, BGallery)
+ (Brooklin, KGallery);
+ (Queens, QGallery);
+ (Staten Island, RGallery)
 
-insert into Sala(Nome, NumeroEspositori, Luogo)
-values (New, 6, Manhattan);
-values (Classic, 3, Manhattan);
-values (Modern, 4, Manhattan);
-values (Contemporary, 8, Manhattan);
-values (New, 5, Bronx);
-values (Classic, 2, Bronx);
-values (Modern, 4, Bronx);
-values (Contemporary, 5, Bronx);
-values (New, 7, Brooklin);
-values (Classic, 5, Brooklin);
-values (Modern, 7, Brooklin);
-values (Contemporary, 4, Brooklin);
-values (New, 2, Queens);
-values (Classic, 3, Queens);
-values (Modern, 3, Queens);
-values (Contemporary, 4, Queens);
-values (New, 8, Staten Island);
-values (Classic, 9, Staten Island);
-values (Modern, 7, Staten Island);
-values (Contemporary, 5, Staten Island)
+insert into Sala(Nome, NumeroEspositori, Luogo) values
+ (New, 6, Manhattan);
+ (Classic, 3, Manhattan);
+ (Modern, 4, Manhattan);
+ (Contemporary, 8, Manhattan);
+ (New, 5, Bronx);
+ (Classic, 2, Bronx);
+ (Modern, 4, Bronx);
+ (Contemporary, 5, Bronx);
+ (New, 7, Brooklin);
+ (Classic, 5, Brooklin);
+ (Modern, 7, Brooklin);
+ (Contemporary, 4, Brooklin);
+ (New, 2, Queens);
+ (Classic, 3, Queens);
+ (Modern, 3, Queens);
+ (Contemporary, 4, Queens);
+ (New, 8, Staten Island);
+ (Classic, 9, Staten Island);
+ (Modern, 7, Staten Island);
+ (Contemporary, 5, Staten Island)
 
-insert into Artista (NickName, DataNascita, Nome)
-values (Ronst, 1999-12-01, Richard);
-values (Milok, 1978-08-27, Mikael);
+insert into Artista (NickName, DataNascita, Nome) values
+ (Ronst, 1999-12-01, Richard);
+ (Milok, 1978-08-27, Mikael);
+ (Rammon, 2000-07-25, Edward);
+ (NWY, 1995-04-07, Soph);
+ (Nalala, 1950-03-13, Nala);
+ (Maria Ross, 1967-05-22, Maria);
+
+ insert into Opera(ID, Titolo, Descrizione, Tipo, Luogo, Nome, Autore) values
+ (01, Le Nuit, )
 
 --Query
 
